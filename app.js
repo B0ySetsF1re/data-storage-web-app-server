dca57e40369d8d369a272f211edf6ae4f935acea
B0ySetsF1re <montersberg@gmail.com>
2020-10-02 15:52:55 +0300

    changed namings

--- app.js
+++ app.js
@@ -10,13 +10,13 @@ const path = require('path');
 const app = express();
 
 const routes = require('./routes/index');
-const files = require('./routes/files');
+const files = require('./routes/data-storage');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/', routes);
-app.use('/api/files', files);
+app.use('/api/data-storage', files);
 
 app.listen(process.env.PORT, process.env.HOST);
 console.log(getCurrTimeConsole() + 'Server started on port ' + process.env.PORT);
--- routes/data-storage.js
+++ routes/data-storage.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const fileController = require('../controllers/fileController');
+const fileController = require('../controllers/dataStorageController');
 
 module.exports = router;
