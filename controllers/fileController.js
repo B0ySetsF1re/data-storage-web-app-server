46464d23e0ff2d57dafbbe74b85ca087a56ba288
B0ySetsF1re <montersberg@gmail.com>
2020-10-02 13:10:17 +0300

    connected cassandra database

---
+++ controllers/fileController.js
@@ -0,0 +1,18 @@
+const getCurrentTime = require('../lib/debuggingTools/getCurrentTime/index');
+
+const asyncForEach = require('../lib/asyncForEach/index');
+
+const cassandra = require('cassandra-driver');
+
+const client = new cassandra.Client({
+  contactPoints: [process.env.HOST],
+  keyspace: process.env.DB_KEYSPACE,
+  localDataCenter: process.env.DB_DATACENTER
+});
+
+client.connect((err, result) => {
+  if(err) {
+    console.log(err);
+  }
+  console.log(getCurrentTime() + 'API: cassandra connected');
+});
--- routes/files.js
+++ routes/files.js
@@ -1,4 +1,5 @@
 const express = require('express');
 const router = express.Router();
+const fileController = require('../controllers/fileController');
 
 module.exports = router;
