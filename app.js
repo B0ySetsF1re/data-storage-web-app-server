9a76dfebb7b597f882da6a54e1446fdb3764139a
B0ySetsF1re <montersberg@gmail.com>
2020-10-02 11:34:48 +0300

    added new api router

--- app.js
+++ app.js
@@ -8,15 +8,13 @@ const path = require('path');
 const app = express();
 
 const routes = require('./routes/index');
+const files = require('./routes/files');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/', routes);
-
-app.get('/', (req, res) => {
-  res.send('Home page...');
-});
+app.use('/api/files', files);
 
 app.listen(process.env.PORT, process.env.HOST);
 console.log('Server started on port ' + process.env.PORT);
