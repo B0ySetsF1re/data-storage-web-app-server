a2a1bb386400f9b95f65e0603f221eac304fa938
B0ySetsF1re <montersberg@gmail.com>
2020-10-02 18:27:08 +0300

    added new temporarily empty post route responsible for upploading data

--- routes/data-storage.js
+++ routes/data-storage.js
@@ -2,4 +2,8 @@ const express = require('express');
 const router = express.Router();
 const fileController = require('../controllers/dataStorageController');
 
+router.post('/upload-file', (req, res) => {
+
+});
+
 module.exports = router;
