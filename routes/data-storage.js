d8db1208dc5e8edabd4ce3c85682b2788083d86d
B0ySetsF1re <montersberg@gmail.com>
2020-10-03 15:28:13 +0300

    created APIs list JSON interface

--- routes/data-storage.js
+++ routes/data-storage.js
@@ -3,6 +3,19 @@ const router = express.Router();
 const dataStorageController = require('../controllers/dataStorageController');
 const { Readable } = require('stream');
 
+router.get('/', (req, res) => {
+  res.json({
+    "APIs": {
+      "POST": {
+        "upload-file": "/api/data-storage/upload-file"
+      },
+      "GET": {
+        "APIs main page" : "/api/data-storage"
+      }
+    }
+  });
+});
+
 router.post('/upload-file', dataStorageController.uploadFile);
 
 module.exports = router;
