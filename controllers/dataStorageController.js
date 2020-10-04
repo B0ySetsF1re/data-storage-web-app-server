d7b9c5fa26bc3b77993d7ac70bcd67f2a74461df
B0ySetsF1re <montersberg@gmail.com>
2020-10-04 21:16:14 +0300

    removed old variables holding cassandra queries

--- controllers/dataStorageController.js
+++ controllers/dataStorageController.js
@@ -12,23 +12,6 @@ const client = new cassandra.Client({
   localDataCenter: process.env.DB_DATACENTER
 });
 
-const createKeySpace = 'CREATE KEYSPACE IF NOT EXISTS ' + process.env.DB_KEYSPACE +
-      ' WITH replication = {\'class\': \'SimpleStrategy\', \'replication_factor\': 3}';
-
-const createFilesMetaDataTable = 'CREATE TABLE IF NOT EXISTS ' + process.env.DB_KEYSPACE +
-    '.files_metadata (object_id uuid, file_name text, disposition text, type text, length double, upload_date date, upload_time time, PRIMARY KEY(object_id, file_name))';
-
-const crateFilesDataTable = 'CREATE TABLE IF NOT EXISTS ' + process.env.DB_KEYSPACE +
-    '.files_data (object_id uuid, chunk_id int, data blob, PRIMARY KEY(object_id, chunk_id))';
-
-const upsertFileMetaData = 'INSERT INTO ' + process.env.DB_KEYSPACE +
-    '.files_metadata (object_id, file_name, disposition, type, length, upload_date, upload_time) VALUES (?, ?, ?, ?, ?, ?, ?)';
-
-const upsertFileData = 'INSERT INTO ' + process.env.DB_KEYSPACE +
-    '.files_data (object_id, chunk_id, data) VALUES (?, ?, ?)';
-
-const selectFileChunks = 'SELECT object_id, chunk_id, data FROM ' + process.env.DB_KEYSPACE + '.files_data WHERE object_id = ?';
-
 client.connect()
   .then(() => {
     return client.execute(queries.createKeySpace);
