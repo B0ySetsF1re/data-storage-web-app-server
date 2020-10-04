5bf5fe567cf375eb25f3b77d1bd7c92c79e23e70
B0ySetsF1re <montersberg@gmail.com>
2020-10-04 19:49:56 +0300

    removed function

--- controllers/dataStorageController.js
+++ controllers/dataStorageController.js
@@ -148,7 +148,6 @@ const uploadFile = async (req, res) => {
 
         } else {
           console.log(err)
-          //res.json({ 'status': 'Error uploading file!' });
         }
       });
     });
@@ -199,42 +198,5 @@ const downloadFile = async (req, res) => {
     });
 }
 
-const uploadFileChunks = async (req, res) => {
-  const uuid = cassandra.types.uuid();
-  const date = cassandra.types.LocalDate.now();
-
-  const stream = createReadStream(resolve(__dirname + '../../files/' + 'big_file.jpg'), {
-    'encoding': 'base64',
-    'highWaterMark': 128 * 1024
-  });
-
-  let counter = 0;
-
-  stream.on('data', (data) => {
-    const objBuffer = new Buffer.from(data, 'base64');
-    // const objBuffer = new Buffer.alloc(Buffer.byteLength(content, 'base64'), content, 'base64');
-    const params = [uuid, counter++, 'big_file.jpg', Buffer.byteLength(content, 'base64'), date, time, objBuffer];
-
-    client.execute(upsertFile, params, { prepare: true }, (err, result) => {
-      if(!err) {
-        console.log(getCurrTimeConsole() + 'Chunk has been upploaded...');
-        //res.json({ 'status': 'uploaded' });
-        console.log({ 'status': 'uploaded' });
-      } else {
-        console.log(err)
-        //res.json({ 'status': 'Error uploading file!' });
-        console.log({ 'status': 'Error uploading file!' });
-      }
-    });
-  });
-
-  stream.on('end', () => {
-    console.log(getCurrTimeConsole() + 'Stream ended...');
-    //res.json({ 'status': 'uploaded' });
-    console.log({ 'status': 'chunk uploaded' });
-  });
-}
-
 exports.uploadFile = uploadFile;
-exports.uploadFileChunks = uploadFileChunks;
 exports.downloadFile = downloadFile;
