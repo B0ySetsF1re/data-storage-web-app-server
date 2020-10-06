d4d5814cef2f07169490bfe99b82c1cc7d68adea
B0ySetsF1re <montersberg@gmail.com>
2020-10-06 19:36:28 +0300

    removed "multiparty" module, as it is not needed by the router

--- routes/data-storage.js
+++ routes/data-storage.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const multiparty = require('multiparty');
 const dataStorageController = require('../controllers/dataStorageController');
 
 router.get('/', (req, res) => {
