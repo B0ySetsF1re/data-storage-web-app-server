9e9fe970afc56f5e7d62cdcffeab8fed48943dc5
B0ySetsF1re <montersberg@gmail.com>
2020-10-20 20:17:16 +0300

    turned queries model to function-constructor implementation instead of constant object literal

--- models/dataStorageQueriesModel.js
+++ models/dataStorageQueriesModel.js
@@ -1,36 +1,37 @@
-module.exports = {
-  createKeySpace: 'CREATE KEYSPACE IF NOT EXISTS ' + process.env.DB_KEYSPACE +
-        ' WITH replication = {\'class\': \'SimpleStrategy\', \'replication_factor\': 3}',
+module.exports = function QueriesModel(KEYSPACE) {
+  if(!new.target) {
+    return new QueriesModel(KEYSPACE);
+  }
 
-  createFilesMetaDataTable: 'CREATE TABLE IF NOT EXISTS ' + process.env.DB_KEYSPACE +
-      '.files_metadata (object_id uuid, file_name text, disposition text, type text, extension text, length double, upload_date date, upload_time time, PRIMARY KEY(object_id))',
+  this.createKeySpace = 'CREATE KEYSPACE IF NOT EXISTS ' + KEYSPACE + ' WITH replication = {\'class\': \'SimpleStrategy\', \'replication_factor\': 3}';
 
-  crateFilesDataTable: 'CREATE TABLE IF NOT EXISTS ' + process.env.DB_KEYSPACE +
-      '.files_data (object_id uuid, chunk_id int, data blob, PRIMARY KEY(object_id, chunk_id))',
+  this.createFilesMetaDataTable = 'CREATE TABLE IF NOT EXISTS ' + KEYSPACE +
+      `.files_metadata (object_id uuid, file_name text, disposition text, type text,
+        extension text, length double, upload_date date, upload_time time, PRIMARY KEY(object_id))`;
 
-  upsertFileMetaData: 'INSERT INTO ' + process.env.DB_KEYSPACE +
-      '.files_metadata (object_id, file_name, disposition, type, extension, length, upload_date, upload_time) VALUES (?, ?, ?, ?, ?, ?, ?, ?)',
+  this.crateFilesDataTable = 'CREATE TABLE IF NOT EXISTS ' + KEYSPACE + '.files_data (object_id uuid, chunk_id int, data blob, PRIMARY KEY(object_id, chunk_id))';
 
-  upsertFileData: 'INSERT INTO ' + process.env.DB_KEYSPACE +
-      '.files_data (object_id, chunk_id, data) VALUES (?, ?, ?)',
+  this.upsertFileMetaData = 'INSERT INTO ' + KEYSPACE + '.files_metadata (object_id, file_name, disposition, type, extension, length, upload_date, upload_time) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
 
-  selectFileMetaData: 'SELECT * FROM ' + process.env.DB_KEYSPACE + '.files_metadata WHERE object_id = ?',
+  this.upsertFileData = 'INSERT INTO ' + KEYSPACE + '.files_data (object_id, chunk_id, data) VALUES (?, ?, ?)';
 
-  selectFileChunks: 'SELECT * FROM ' + process.env.DB_KEYSPACE + '.files_data WHERE object_id = ?',
+  this.selectFileMetaData = 'SELECT * FROM ' + KEYSPACE + '.files_metadata WHERE object_id = ?';
 
-  selectAllMetaDataContent: 'SELECT * FROM ' + process.env.DB_KEYSPACE + '.files_metadata',
+  this.selectFileChunks = 'SELECT * FROM ' + KEYSPACE + '.files_data WHERE object_id = ?';
 
-  selectAllFileExtensionsAndLength: 'SELECT extension, length FROM ' + process.env.DB_KEYSPACE +  '.files_metadata',
+  this.selectAllMetaDataContent = 'SELECT * FROM ' + KEYSPACE + '.files_metadata';
 
-  selectFileMetaDataNameAndDisposition: 'SELECT file_name, disposition, extension FROM ' + process.env.DB_KEYSPACE + '.files_metadata WHERE object_id = ?',
+  this.selectAllFileExtensionsAndLength = 'SELECT extension, length FROM ' + KEYSPACE +  '.files_metadata';
 
-  updateFileMetaDataNameAndDisposition: 'UPDATE ' + process.env.DB_KEYSPACE + '.files_metadata SET file_name = ?, disposition = ? WHERE object_id = ?',
+  this.selectFileMetaDataNameAndDisposition = 'SELECT file_name, disposition, extension FROM ' + KEYSPACE + '.files_metadata WHERE object_id = ?';
 
-  deleteFileMetaDataContent: 'DELETE FROM ' + process.env.DB_KEYSPACE + '.files_metadata WHERE object_id = ?',
+  this.updateFileMetaDataNameAndDisposition = 'UPDATE ' + KEYSPACE + '.files_metadata SET file_name = ?, disposition = ? WHERE object_id = ?';
 
-  deleteFileDataContent: 'DELETE FROM ' + process.env.DB_KEYSPACE + '.files_data WHERE object_id = ?',
+  this.deleteFileMetaDataContent = 'DELETE FROM ' + KEYSPACE + '.files_metadata WHERE object_id = ?';
 
-  deleteAllFilesMetaDataContent: 'TRUNCATE ' + process.env.DB_KEYSPACE + '.files_metadata',
+  this.deleteFileDataContent = 'DELETE FROM ' + KEYSPACE + '.files_data WHERE object_id = ?';
 
-  deleteAllFilesDataContent: 'TRUNCATE ' + process.env.DB_KEYSPACE + '.files_data'
+  this.deleteAllFilesMetaDataContent = 'TRUNCATE ' + KEYSPACE + '.files_metadata';
+
+  this.deleteAllFilesDataContent = 'TRUNCATE ' + KEYSPACE + '.files_data';
 }
